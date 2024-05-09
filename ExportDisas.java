7e0560fcddc82c87cc7e780ba1b487aa908f012d
blend-tea <161995054+blend-tea@users.noreply.github.com>
2024-05-09 16:14:39 +0900

    Update ExportDisas.java

--- ExportDisas.java
+++ ExportDisas.java
@@ -7,7 +7,8 @@
 
 import java.util.*;
 import java.io.*;
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import docking.widgets.filechooser.GhidraFileChooser;
 import ghidra.app.script.GhidraScript;
@@ -19,81 +20,85 @@ import ghidra.program.model.listing.FunctionIterator;
 import ghidra.program.model.listing.Instruction;
 
 public class ExportDisas extends GhidraScript {
-	Set<String> written = new HashSet<String>();
-	Set<String> notWritten = new HashSet<String>();
-	Map<String, Address> funcs = new HashMap<String, Address>();
-	
-	
-	@Override
-	protected void run() throws Exception {
-		//TODO: Add script code here
-		GhidraFileChooser fileChooser = new GhidraFileChooser(null);
-		fileChooser.setTitle("Save Disassembly");
-		
-		File selectedFile = fileChooser.getSelectedFile();
-		if(selectedFile == null) {
-			println("No file selected.");
-			return;
-		}
-		
-		String outputPath = selectedFile.getAbsolutePath();
-		println(outputPath);
-		
-		PrintWriter pwriter = new PrintWriter(new BufferedWriter(new FileWriter(outputPath)));
-		try {
-			Address pl = currentLocation.getAddress();
-			Program program = currentProgram;
-			FunctionIterator funcIter = program.getFunctionManager().getFunctions(true);
-			while (funcIter.hasNext()) {
-				Function func = funcIter.next();
-				funcs.put(func.getName(), func.getEntryPoint());
-			}
-			Function function = program.getFunctionManager().getFunctionContaining(pl);
-			if (function != null) {
-				Address[] startAddr = {function.getEntryPoint()};
-				exportAsm(startAddr, pwriter);
-			} else {
-				println("No function found at the current location.");
-			}
-		} finally {
-			pwriter.close();
-		}
-	}
-	
-	private void exportAsm(Address[] funcAddrs, PrintWriter pwriter) throws Exception {
-		ArrayList<Address> writeList = new ArrayList<Address>();
-		for (Address funcAddr : funcAddrs) {
-			Function func = getFunctionAt(funcAddr);
-			if (func != null && !written.contains(func.getName())) {
-				written.add(func.getName());
-				pwriter.println(func.getName() + "," + func.getEntryPoint());
-				println(func.getName() + "," + func.getEntryPoint());
-				Address endAddr = func.getBody().getMaxAddress();
-				for(Address addr = funcAddr; addr.compareTo(endAddr) <= 0; addr = getInstructionAfter(addr).getAddress()) {
-					Instruction inst = getInstructionAt(addr);
-					if(inst == null) break;
-					pwriter.println(inst.toString());
-					println(inst.toString());
-					if(inst.getMnemonicString().equals("CALL")) {
-						Reference ref[]  = inst.getOperandReferences(0);
-						if(ref.length != 0) {
-							Address callAddr = ref[0].getToAddress();
-							Function callFunc = getFunctionAt(callAddr);
-							if (callFunc != null) {
-								String FuncName = callFunc.getName();
-								if (!written.contains(FuncName)) {
-									writeList.add(callAddr);
-								}
-							}
-						}
-					}
-				}
-				pwriter.println("");
-				println("");
-			}
-		}
-		if (writeList.size() > 0) {
-			exportAsm(writeList.toArray(new Address[writeList.size()]), pwriter);
-		}
-	}
+    Set<String> written = new HashSet<String>();
+    Set<String> notWritten = new HashSet<String>();
+    Map<String, Address> funcs = new HashMap<String, Address>();
+
+    @Override
+    protected void run() throws Exception {
+        GhidraFileChooser fileChooser = new GhidraFileChooser(null);
+        fileChooser.setTitle("Save Disassembly");
+
+        File selectedFile = fileChooser.getSelectedFile();
+        if(selectedFile == null) {
+            println("No file selected.");
+            return;
+        }
+
+        String outputPath = selectedFile.getAbsolutePath();
+        println(outputPath);
+
+        JSONArray jsonList = new JSONArray();
+
+        try (PrintWriter pwriter = new PrintWriter(new BufferedWriter(new FileWriter(outputPath)))) {
+            Address pl = currentLocation.getAddress();
+            Program program = currentProgram;
+            FunctionIterator funcIter = program.getFunctionManager().getFunctions(true);
+            while (funcIter.hasNext()) {
+                Function func = funcIter.next();
+                funcs.put(func.getName(), func.getEntryPoint());
+            }
+            Function function = program.getFunctionManager().getFunctionContaining(pl);
+            if (function != null) {
+                Address[] startAddr = {function.getEntryPoint()};
+                exportAsm(startAddr, jsonList);
+            } else {
+                println("No function found at the current location.");
+            }
+
+            // Write JSON to file
+            pwriter.write(jsonList.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void exportAsm(Address[] funcAddrs, JSONArray jsonList) throws Exception {
+        ArrayList<Address> writeList = new ArrayList<Address>();
+        for (Address funcAddr : funcAddrs) {
+            Function func = getFunctionAt(funcAddr);
+            if (func != null && !written.contains(func.getName())) {
+                written.add(func.getName());
+
+                JSONObject funcObj = new JSONObject();
+                JSONArray instArray = new JSONArray();
+                Address endAddr = func.getBody().getMaxAddress();
+                for(Address addr = funcAddr; addr.compareTo(endAddr) <= 0; addr = getInstructionAfter(addr).getAddress()) {
+                    Instruction inst = getInstructionAt(addr);
+                    if(inst == null) break;
+                    instArray.add(inst.toString());
+
+                    if(inst.getMnemonicString().equals("CALL")) {
+                        Reference ref[]  = inst.getOperandReferences(0);
+                        if(ref.length != 0) {
+                            Address callAddr = ref[0].getToAddress();
+                            Function callFunc = getFunctionAt(callAddr);
+                            if (callFunc != null) {
+                                String FuncName = callFunc.getName();
+                                if (!written.contains(FuncName)) {
+                                    writeList.add(callAddr);
+                                }
+                            }
+                        }
+                    }
+                }
+                funcObj.put("function_name", func.getName());
+                funcObj.put("instruction", instArray);
+                jsonList.add(funcObj);
+            }
+        }
+        if (writeList.size() > 0) {
+            exportAsm(writeList.toArray(new Address[writeList.size()]), jsonList);
+        }
+    }
 }
